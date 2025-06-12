32e4dfc640baddb41e6509135b5fba1b99128f08
Valerianavo <lilaj2sy@gmail.com>
2025-06-12 12:03:23 -0500

    menu-nav-responsive-finally

--- script.js
+++ script.js
@@ -1,2 +1,31 @@
+//menu para telefono
+function copyMenu(){
+    //inside .dpt-cat to .departaments
+    let dptCategory = document.querySelector('.dpt-menu');
+    let dptPlace = document.querySelector('.departments');
+    dptPlace.innerHTML = dptCategory.innerHTML;
 
+    //inside nav to nav
+    let mainNav = document.querySelector('.header-nav nav');
+    let navPlace = document.querySelector('.off-canvas nav');
+    navPlace.innerHTML = mainNav.innerHTML;
+
+    //.header-top.wrapper to .thetop-nav
+    let topNav = document.querySelector('.header-top .wrapper');
+    let topPlace = document.querySelector('.off-canvas .thetop-nav');
+    topPlace.innerHTML = topNav.innerHTML;
+}
+copyMenu();
+
+//menu para celulares burguer
+const menuButton = document.querySelector('.trigger'), 
+    closeButton = document.querySelector('.t-close'),
+    addClass = document.querySelector('.site');
+menuButton.addEventListener('click',function(){
+    addClass.classList.toggle('showmenu');
+})
+
+closeButton.addEventListener('click', function(){
+    addClass.classList.remove('showmenu')
+})
 
